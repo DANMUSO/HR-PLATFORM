c38a222967e3c4ac742b8e7d7fb595a5ed957374
Danmuso <kimdan2030@gmail.com>
2024-04-12 18:15:35 +0300

    HR

---
+++ HRPLATFORM/resources/views/layouts/app.blade.php
@@ -0,0 +1,343 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Dashboard V.1 | Nalika - Material Admin Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+		============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <!-- Google Fonts
+		============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/bootstrap.min.css')}}">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/font-awesome.min.css')}}">
+	<!-- nalika Icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/nalika-icon.css')}}">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{ asset('v1/css/owl.theme.css')}}">
+    <link rel="stylesheet" href="{{ asset('v1/css/owl.transitions.css')}}">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/animate.css')}}">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/normalize.css')}}">
+    <!-- meanmenu icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/meanmenu.min.css')}}">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/main.css')}}">
+    <!-- morrisjs CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/morrisjs/morris.css')}}">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/scrollbar/jquery.mCustomScrollbar.min.css')}}">
+    <!-- metisMenu CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/metisMenu/metisMenu.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('v1/css/metisMenu/metisMenu-vertical.css')}}">
+    <!-- calendar CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/calendar/fullcalendar.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('v1/css/calendar/fullcalendar.print.min.css')}}">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/style.css')}}">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{ asset('v1/css/responsive.css')}}">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="{{ asset('v1/js/vendor/modernizr-2.8.3.min.js')}}"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <div class="left-sidebar-pro">
+        <nav id="sidebar" class="">
+            <div class="sidebar-header">
+                <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                <strong><img src="img/logo/logosn.png" alt="" /></strong>
+            </div>
+			<div class="nalika-profile">
+				<div class="profile-dtl">
+					<a href="#"></a>
+					<h2>HR PLATFORM</h2>
+				</div>
+			</div>
+            <div class="left-custom-menu-adp-wrap comment-scrollbar">
+                <nav class="sidebar-nav left-sidebar-menu-pro">
+                    <ul class="metismenu" id="menu1">
+                        <li class="active">
+                            <a class="has-arrow" href="#">
+								   <i class="icon nalika-home icon-wrap"></i>
+								   <span class="mini-click-non">Dashboard</span>
+								</a>
+                            
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-table icon-wrap"></i> <span class="mini-click-non">Users Mgt</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="Inbox" href=""><span class="mini-sub-pro">Users1</span></a></li>
+                              
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-diamond icon-wrap"></i> <span class="mini-click-non">Companies Mgt</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="Google Map" href=""><span class="mini-sub-pro">Companies</span></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-pie-chart icon-wrap"></i> <span class="mini-click-non">Vacancies Mgt</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="File Manager" href=""><span class="mini-sub-pro">Vacancies</span></a></li>
+                              
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-bar-chart icon-wrap"></i> <span class="mini-click-non">Applicants Mgt</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="Bar Charts" href="#"><span class="mini-sub-pro">New Applicants</span></a></li>
+                                <li><a title="Bar Charts" href="#"><span class="mini-sub-pro">Shortlisted Apps</span></a></li>
+                                <li><a title="Bar Charts" href="#"><span class="mini-sub-pro">Qualified Apps</span></a></li>
+                                <li><a title="Bar Charts" href="#"><span class="mini-sub-pro">Pipeline</span></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-pie-chart icon-wrap"></i> <span class="mini-click-non">Vacancies Mgt</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="File Manager" href=""><span class="mini-sub-pro">Vacancies</span></a></li>
+                              
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-bar-chart icon-wrap"></i> <span class="mini-click-non">Applicants Mgt</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="Bar Charts" href="#"><span class="mini-sub-pro">New Applicants</span></a></li>
+                                <li><a title="Bar Charts" href="#"><span class="mini-sub-pro">Shortlisted Apps</span></a></li>
+                                <li><a title="Bar Charts" href="#"><span class="mini-sub-pro">Qualified Apps</span></a></li>
+                                <li><a title="Bar Charts" href="#"><span class="mini-sub-pro">Pipeline</span></a></li>
+                            </ul>
+                        </li>
+                        
+                       
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+    </div>
+    <!-- Start Welcome area -->
+    <div class="all-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="logo-pro">
+                        <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-advance-area">
+            <div class="header-top-area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="header-top-wraper">
+                                <div class="row">
+                                    <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12">
+                                        <div class="menu-switcher-pro">
+                                            <button type="button" id="sidebarCollapse" class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
+													<i class="icon nalika-menu-task"></i>
+												</button>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-7 col-sm-6 col-xs-12">
+                                        <div class="header-top-menu tabl-d-n hd-search-rp">
+                                            <div class="breadcome-heading">
+												<form role="search" class="">
+													<input type="text" placeholder="Search..." class="form-control">
+													<a href=""><i class="fa fa-search"></i></a>
+												</form>
+											</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                                        <div class="header-right-info">
+                                            <ul class="nav navbar-nav mai-top-nav header-right-menu">
+
+
+                                                <li class="nav-item">
+                                                    <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">
+															<i class="icon nalika-user"></i>
+															<span class="admin-name">Administrator</span>
+															<i class="icon nalika-down-arrow nalika-angle-dw"></i>
+														</a>
+                                                    <ul role="menu" class="dropdown-header-top author-log dropdown-menu animated zoomIn">
+                                                        
+                                                        <li>
+                                                        <a class="nav-link" href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+
+                                                        <p> 
+                                                        {{ __('Logout') }}
+                                                        </p>
+                                                        </a>
+
+                                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                            @csrf
+                                                        </form>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu start -->
+            <div class="mobile-menu-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="mobile-menu">
+                                <nav id="dropdown">
+                                    <ul class="mobile-menu-nav">
+                                        <li><a data-toggle="collapse" data-target="#Charts" href="#">Dashboard <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                            
+                                        </li>
+                                        <li><a data-toggle="collapse" data-target="#demo" href="#">Users Mgt <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                            <ul id="demo" class="collapse dropdown-header-top">
+                                                <li><a href="#">Users</a>
+                                                </li>
+                                               
+                                            </ul>
+                                        </li>
+                                        <li><a data-toggle="collapse" data-target="#others" href="#">Companies Mgt <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                            <ul id="others" class="collapse dropdown-header-top">
+                                                <li><a href="#">Companies</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a data-toggle="collapse" data-target="#Miscellaneousmob" href="#">Vacancies Mgt <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                            <ul id="Miscellaneousmob" class="collapse dropdown-header-top">
+                                                <li><a href="#">Vacancies</a>
+                                                </li>
+                                               
+                                            </ul>
+                                        </li>
+                                        <li><a data-toggle="collapse" data-target="#Chartsmob" href="#">Applicants <span class="admin-project-icon nalika-icon nalika-down-arrow"></span></a>
+                                            <ul id="Chartsmob" class="collapse dropdown-header-top">
+                                                <li><a href="#">New Apps</a>
+                                                </li>
+                                                <li><a href="#">Shortlisted Apps</a>
+                                                </li>
+                                                <li><a href="#">Qualified Apps</a>
+                                                </li>
+                                                <li><a href="#">Pipeline</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu end -->
+            
+        </div>
+
+        @yield('content')
+
+        
+
+        <div class="footer-copyright-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="footer-copy-right">
+                            <p>Copyright © 2018 <a href="https://colorlib.com/wp/templates/">Colorlib</a> All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- jquery
+		============================================ -->
+    <script src="{{ asset('v1/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="{{ asset('v1/js/bootstrap.min.js')}}"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="{{ asset('v1/js/wow.min.js')}}"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="{{ asset('v1/js/jquery-price-slider.js')}}"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="{{ asset('v1/js/jquery.meanmenu.js')}}"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="{{ asset('v1/js/owl.carousel.min.js')}}"></script>
+    <!-- sticky JS
+		============================================ -->
+    <script src="{{ asset('v1/js/jquery.sticky.js')}}"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="{{ asset('v1/js/jquery.scrollUp.min.js')}}"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="{{ asset('v1/js/scrollbar/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <script src="{{ asset('v1/js/scrollbar/mCustomScrollbar-active.js')}}"></script>
+    <!-- metisMenu JS
+		============================================ -->
+    <script src="{{ asset('v1/js/metisMenu/metisMenu.min.js')}}"></script>
+    <script src="{{ asset('v1/js/metisMenu/metisMenu-active.js')}}"></script>
+    <!-- sparkline JS
+		============================================ -->
+    <script src="{{ asset('v1/js/sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{ asset('v1/js/sparkline/jquery.charts-sparkline.js')}}"></script>
+    <!-- calendar JS
+		============================================ -->
+    <script src="{{ asset('v1/js/calendar/moment.min.js')}}"></script>
+    <script src="{{ asset('v1/js/calendar/fullcalendar.min.js')}}"></script>
+    <script src="{{ asset('v1/js/calendar/fullcalendar-active.js')}}"></script>
+	<!-- float JS
+		============================================ -->
+    <script src="{{ asset('v1/js/flot/jquery.flot.js')}}"></script>
+    <script src="{{ asset('v1/js/flot/jquery.flot.resize.js')}}"></script>
+    <script src="{{ asset('v1/js/flot/curvedLines.js')}}"></script>
+    <script src="{{ asset('v1/js/flot/flot-active.js')}}"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="{{ asset('v1/js/plugins.js')}}"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="{{ asset('v1/js/main.js')}}"></script>
+</body>
+
+</html>
